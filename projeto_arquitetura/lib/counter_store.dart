de082890de2a953cfc36a145c11bfc282d448671
dandy <danielsbarbosa27@gmail.com>
2021-07-08 01:24:58 -0300

    ajutes

---
+++ projeto_arquitetura/lib/counter_store.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_triple/flutter_triple.dart';
+
+class CounterStore extends NotifierStore<Exception, int> {
+  CounterStore() : super(0); 
+}
+
+class NotifierStore {
+}
--- projeto_arquitetura/lib/main.dart
+++ projeto_arquitetura/lib/main.dart
@@ -19,3 +19,14 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
